e67c64061d29329bb94880062b1c9e88228a1cb6
Noyan Kabakcı <noyankabakci@windowslive.com>
2025-10-01 07:05:36 +0300

    Added paging request and started adding seq container

--- src/main/java/com/noanka/swapper/utility/http/request/PagingRequest.java
+++ src/main/java/com/noanka/swapper/utility/http/request/PagingRequest.java
@@ -1,5 +1,22 @@
 package com.noanka.swapper.utility.http.request;
 
 public class PagingRequest {
+    private int dateTime;
+    private int size;
 
+    public int getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(int dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
--- src/main/java/com/noanka/swapper/utility/http/response/ResponseListBase.java
+++ src/main/java/com/noanka/swapper/utility/http/response/ResponseListBase.java
@@ -8,12 +8,12 @@ public class ResponseListBase<T> extends PagingResponse{
     private final List<String> userMessage;
     private final boolean success;
 
-    public ResponseListBase(List<T> data, List<String> message, List<String> userMessage, boolean success) {
+    public ResponseListBase(List<T> data, List<String> message, List<String> userMessage, boolean success, PagingResponse pagingResponse) {
+        super(pagingResponse.getIndex(), pagingResponse.getSize(), pagingResponse.getPageCount());
         this.data = data;
         this.message = message;
         this.userMessage = userMessage;
         this.success = success;
-
     }
 
     public List<T> getData() {
